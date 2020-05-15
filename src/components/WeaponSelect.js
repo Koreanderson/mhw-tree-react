e670b8c53ba0fea0015f9e797a82c9076ded4df7
brian <koreanderson@gmail.com>
2020-05-14 22:35:03 -0400

    add buildUrl method

--- src/components/WeaponSelect.js
+++ src/components/WeaponSelect.js
@@ -4,27 +4,36 @@ import WeaponTypeList from '../components/WeaponTypeList'
 import DamageTypeList from '../components/DamageTypeList'
 
 class WeaponSelect extends React.Component {
+
   constructor(props) {
     super(props);
-    this.buildUrl = this.buildUrl.bind(this);
     this.setWeaponType = this.setWeaponType.bind(this);
     this.setDamageType = this.setWeaponType.bind(this);
+    this.getWeaponResults = this.getWeaponResults.bind(this);
     this.state = {
       weaponType: 'great-sword',
       damageType: 'poison'
     }
   }
+
   render() {
-    console.log(this.state);
+
     const Row = styled.div`
       display: flex;
     `
+
+
     return(
       <Row >
         <WeaponTypeList handleWeaponClick={this.setWeaponType}/>
-        <DamageTypeList handleDamageClick={this.setDamageType} />
+        <DamageTypeList handleDamageClick={this.setDamageType}/>
       </Row>
     );
+
+  }
+
+  componentDidUpdate() {
+    this.getWeaponResults();
   }
 
   setWeaponType(weaponType) {
@@ -35,8 +44,11 @@ class WeaponSelect extends React.Component {
     this.setState({damageType: damageType});
   }
 
-  buildUrl() {
-
+  getWeaponResults() {
+    const damageType = this.state.damageType;
+    const weaponType = this.state.weaponType;
+    const url = 'https://mhw-db.com/weapon?q={"type":"' + weaponType + '","elements.type":"' + damageType + '"}';
+    console.log(url);
   }
 }
 export default WeaponSelect;
