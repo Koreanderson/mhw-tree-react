8b57999f505583b663a604e2734b37ceaa0cec2d
brian <koreanderson@gmail.com>
2020-05-24 17:07:20 -0400

    update DamageTypeList to functional component

--- src/components/DamageTypeList.js
+++ src/components/DamageTypeList.js
@@ -1,65 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
 
-class DamageTypeList extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-
+const DamageTypeList = (props) => {
+  const damageTypes = [
+    'poison',
+    'fire',
+    'water',
+    'thunder',
+    'ice',
+    'dragon',
+    'sleep',
+    'blast',
+    'paralysis'
+  ];
+  const Ul = styled.ul`
+    list-style-type: none;
+    text-align: left;
+    li {
+      cursor: pointer;
+      margin: .5em;
+      padding: .25em .5em;
+    }
+    .selected { 
+      border: 1px solid white;
+    }
+  `
+  const currentDamageType = props.selectedDamageType;
+  const handleClick = (e) => {
     const damageType = e.target.attributes.damageType.value;
     const el = e.target;
     const listItems = document.querySelectorAll('.damage-types--item');
-
-    this.props.handleDamageClick(damageType);
+    props.handleDamageClick(damageType);
     console.log(damageType);
-
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log(this.state);
-  }
+  const damageTypeList = damageTypes.map((damageType,i) => 
+    <li className={"damage-types--item " + (currentDamageType == damageType ? 'selected' : '')} onClick={handleClick} key={i} damagetype={damageType}>{damageType}</li>
+  );
 
-  render() {
-    const damageTypes = [
-      'poison',
-      'fire',
-      'water',
-      'thunder',
-      'ice',
-      'dragon',
-      'sleep',
-      'blast',
-      'paralysis'
-    ];
+  return (
+    <Ul className="damage-types">
+      {damageTypeList}
+    </Ul>
+  );
 
-    const Ul = styled.ul`
-      list-style-type: none;
-      text-align: left;
-      li {
-        cursor: pointer;
-        margin: .5em;
-        padding: .25em .5em;
-      }
-      .selected { 
-        border: 1px solid white;
-      }
-    `
-    const currentDamageType = this.props.selectedDamageType;
-
-    const damageTypeList = damageTypes.map((damageType,i) => 
-      <li className={"damage-types--item " + (currentDamageType == damageType ? 'selected' : '')} onClick={this.handleClick} key={i} damagetype={damageType}>{damageType}</li>
-    );
-
-    return (
-      <Ul className="damage-types">
-        {damageTypeList}
-      </Ul>
-    );
-  }
 }
 
 export default DamageTypeList;
