f3e9f573ad572929cf74cb338f94638505b80607
brian <koreanderson@gmail.com>
2020-05-24 16:56:31 -0400

    update WeaponTypeList and Wishlist to functional components

--- src/components/WeaponTypeList.js
+++ src/components/WeaponTypeList.js
@@ -1,62 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
 
-class WeaponTypeList extends React.Component {
+const WeaponTypeList = (props) => {
+  const weaponTypes = [
+    'great-sword',
+    'long-sword',
+    'sword-and-shield',
+    'dual-blades',
+    'hammer',
+    'hunting-horn',
+    'lance',
+    'gunlance',
+    'switch-axe',
+    'charge-blade',
+    'insect-glaive',
+    'light-bowgun',
+    'heavy-bowgun',
+    'bow'
+  ];
+  const Ul = styled.ul`
+    list-style-type: none;
+    text-align: left;
+    li {
+      cursor: pointer;
+      margin: .5em;
+      padding: .25em .5em;
+    }
+    .selected { 
+      border: 1px solid white;
+    }
+  `
+  const currentWeaponType = props.selectedWeaponType;
 
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
+  const handleClick = (e) => {
     const weaponType = e.target.attributes.weapontype.value;
-    this.props.handleWeaponClick(weaponType);
+    props.handleWeaponClick(weaponType);
   }
 
-  render() {
-    const weaponTypes = [
-      'great-sword',
-      'long-sword',
-      'sword-and-shield',
-      'dual-blades',
-      'hammer',
-      'hunting-horn',
-      'lance',
-      'gunlance',
-      'switch-axe',
-      'charge-blade',
-      'insect-glaive',
-      'light-bowgun',
-      'heavy-bowgun',
-      'bow'
-    ];
-    const Ul = styled.ul`
-      list-style-type: none;
-      text-align: left;
-      li {
-        cursor: pointer;
-        margin: .5em;
-        padding: .25em .5em;
-      }
-      .selected { 
-        border: 1px solid white;
-      }
-    `
-
-    const currentWeaponType = this.props.selectedWeaponType;
-    console.log(this.props.selectedWeaponType);
-
-
-    const weaponTypeList = weaponTypes.map((weaponType,i) => 
-      <li className={(currentWeaponType == weaponType ? 'selected' : '' )} onClick={this.handleClick} key={i} weapontype={weaponType}>{weaponType}</li>
-    );
-
+  const weaponTypeList = weaponTypes.map((weaponType,i) => {
     return (
-      <Ul className="weapon-types">
-        {weaponTypeList}
-      </Ul>
+    <li className={(currentWeaponType == weaponType ? 'selected' : '' )} onClick={handleClick} key={i} weapontype={weaponType}>{weaponType}</li>
     );
-  }
+  
+  });
+
+  return (
+    <Ul className="weapon-types">
+      {weaponTypeList}
+    </Ul>
+  );
 }
 
 export default WeaponTypeList;
--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
-class Wishlist extends React.Component {
-
-  render() {
-    return (
-      <div>
-        Wishlist
-        <ul>
-          <li></li>
-        </ul>
-      </div>
-    );
-  }
-
+const Wishlist = (props) => {
+  return (
+    <div>
+      Wishlist
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
 
 export default Wishlist;
